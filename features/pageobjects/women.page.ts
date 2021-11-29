9b17369465b54164f49f881e5db9755ee182bfcf
farashaa <farashaik13@gmail.com>
2021-11-29 17:12:09 +0530

    second commit

---
+++ features/pageobjects/women.page.ts
@@ -0,0 +1,23 @@
+class Women{
+    get productContainer(){
+        return $("//img[@title='Blouse']/ancestor::div[@class='product-container']")
+    }
+    get addToCart(){
+        return $("//a[@title='Add to cart' and @data-id-product='2']")
+    }
+    async clickAddToCart(){
+        await this.productContainer.scrollIntoView()
+        await this.productContainer.moveTo()
+        await this.addToCart.waitForClickable()
+        await this.addToCart.click()
+    }
+    get productAdded(){
+        return $("//i[@class='icon-ok']/..")
+    }
+    get checkOut() { return $('//a[@title="Proceed to checkout"]') }
+
+    // get checkOut(){
+    //     return $("//a[@class='btn btn-default button button-medium']")
+    // }
+}
+export default new Women()
